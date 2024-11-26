803f0e44fa909fb016972bc5b7cfd1cda128b0b4
Fatal999 <euronymous.ts@yandex.ru>
2024-11-26 15:12:19 +0300

    v2.8 (Added some tablet width)

--- source/js/blocks/juri.js
+++ source/js/blocks/juri.js
@@ -3,12 +3,40 @@ import { Navigation } from 'swiper/modules';
 
 Swiper.use([Navigation]);
 
-new Swiper('.swiper', {
-  loop: true,
+function getSlidesPerView() {
+  if (window.innerWidth >= 1366) {
+    return 4;
+  } else if (window.innerWidth >= 768) {
+    return 2;
+  } else {
+    return 1;
+  }
+}
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+function createSwiper() {
+  return new Swiper('.swiper--juri', {
+    loop: true,
+    slidesPerView: getSlidesPerView(),
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        // spaceBetween: 40
+      },
+      1366: {
+        slidesPerView: 4
+      },
+    },
+  });
+}
+
+let swiper = createSwiper();
+
+window.addEventListener('resize', () => {
+  swiper.destroy(true, true);
+  swiper = createSwiper();
 });
-
